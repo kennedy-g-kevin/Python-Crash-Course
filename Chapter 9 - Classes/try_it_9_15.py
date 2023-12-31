e712123ce9d242c6ae9f1cdc6c07b6b7297b40f6
kevinkennedy <kennedy.g.kevin@gmail.com>
2023-12-31 15:20:28 -0500

    python-crash-course - add some comments.

--- Chapter 9 - Classes/try_it_9_15.py
+++ Chapter 9 - Classes/try_it_9_15.py
@@ -2,8 +2,10 @@ from random import choice
 
 
 def generate_winning_numbers(possible_characters, pool_size):
-    """Generates winning numbers by picking 4 random characters from a given list"""
+    """Generates winning numbers by picking 'pool_size' number of random characters from a given list"""
     winning_numbers = []
+
+    # Pick a random character from the list for number of times specified by pool_size. Return winning number list.
     for number in range(1, pool_size + 1):
         winning_numbers.append(choice(possible_characters))
     return winning_numbers
@@ -17,12 +19,17 @@ def check_if_winner(my_ticket, pool_size):
     iterations = 0
 
     while not winner:
+
+        # Generate the winning numbers for a given pool_size.
         winning_numbers = generate_winning_numbers(['k', 'l', 'e', 'z', 'h', 3, 5, 7, 1, 0, 2, 4, 6, 8, 9], pool_size)
         matching_numbers = 0
+
+        # Iterate through each number of my_ticket and check if it's in winning numbers. Count +1 if match.
         for number in my_ticket:
             if number in winning_numbers:
                 matching_numbers += 1
 
+        # Once number of matches equals pool size, we have a winner.
         if matching_numbers == pool_size:
             winner = True
             print(f"\n\nWinner Winner Chicken Dinner!")
@@ -36,4 +43,5 @@ def check_if_winner(my_ticket, pool_size):
         print(f"\t- Matching Numbers: {matching_numbers}")
 
 
+# Run the function
 check_if_winner(['z', 'e', 'k', 8, 4, 0], 6)
