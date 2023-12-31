20f308f15de970d70fdb5ee531183f8537182646
kevinkennedy <kennedy.g.kevin@gmail.com>
2023-12-31 13:27:13 -0500

    python-crash-course - my_two_cars.py in chapter 9.

---
+++ Chapter 9 - Classes/car_class (parent & child)/my_two_cars.py
@@ -0,0 +1,6 @@
+import car
+
+my_car = car.Car('Honda', 'Civic', 2024)
+my_electric_car = car.ElectricCar('Tesla', 'Model S', 2023)
+print(my_car.get_descriptive_name())
+print(my_electric_car.get_descriptive_name())
